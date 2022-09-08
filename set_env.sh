4343578c79b17fd5487d33ad0ec1d89ca5f941d5
kens3dola <sangpham.quang.79@gmail.com>
2022-09-08 13:01:39 +0700

    initial commit

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+set POSTGRES_USERNAME=postgres
+set POSTGRES_PASSWORD=anhsang79
+set POSTGRES_HOST=database-2.cuorgl3emi8y.us-east-1.rds.amazonaws.com
+set POSTGRES_DB=postgres
+set AWS_BUCKET=arn:aws:s3:::sangpq-cloud-dev-bucket
+set AWS_REGION=us-east-1
+set AWS_PROFILE=default
+set JWT_SECRET=testing
+set URL=http://localhost:8100
