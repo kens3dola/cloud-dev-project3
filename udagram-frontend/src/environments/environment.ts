5799d81d07b2bf3322c8b6ebdd4a90e8243c6968
kens3dola <sangpham.quang.79@gmail.com>
2022-09-10 13:06:31 +0700

    GG

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   appName: "Udagram",
   apiHost:
-    "a8224ed0554f24c3e93fae64999ee8a1-2074227809.us-east-1.elb.amazonaws.com:8080/api/v0",
+    "a9bab0e3372fc4d7d8a0baf532e54692-1407931521.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
